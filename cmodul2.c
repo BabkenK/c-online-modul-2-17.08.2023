c0fb69da9ae1d5695f8dc29291ebe8d3f1b88084
BabkenK <139691909+BabkenK@users.noreply.github.com>
2023-08-17 21:45:53 +0400

    Add files via upload
    Int, double, char Հայտարարել փոփոխական, որի արժեքը մուտքգրվելու է օգտվողի կողմից։

---
+++ cmodul2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main ()
+{
+	int x;
+	double e;
+	char y;
+
+	printf("Enter value:\n");
+	scanf("%d", &x);
+	
+	printf("Enter value:\n ");
+	scanf ("%lf",  &e);
+	
+
+	printf ("Entere value: \n");
+	scanf("%c", &y);
+
+	return 0;
+}
